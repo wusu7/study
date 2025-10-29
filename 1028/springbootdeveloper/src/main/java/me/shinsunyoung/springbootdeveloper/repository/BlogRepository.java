69b3b7623ed79eaf88dd582480d3fce8b8479fcb
wusu7 <chldntn7@gmail.com>
2025-10-29 16:28:44 +0900

    spring

--- 1028/springbootdeveloper/src/main/java/me/shinsunyoung/springbootdeveloper/repository/BlogRepository.java
+++ 1028/springbootdeveloper/src/main/java/me/shinsunyoung/springbootdeveloper/repository/BlogRepository.java
@@ -1,4 +1,9 @@
 package me.shinsunyoung.springbootdeveloper.repository;
 
-public class BlogRepository {
+import me.shinsunyoung.springbootdeveloper.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BlogRepository extends JpaRepository<Article, Long> {
 }
